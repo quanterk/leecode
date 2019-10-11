bbdc7a20b3cb95f4e08ca396d24df306cab1cb17
quanterk <51376997+quanterk@users.noreply.github.com>
2019-10-11 23:28:27 +0800

    242

---
+++ 242.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include<vector>
+#include <algorithm>
+#include<string>
+#include<map>
+using namespace std;
+
+
+class Solution {
+public:
+	bool isAnagram(string s, string t) {
+		if (s.size() != t.size()) return false;
+		if (s == t) return true;
+		int x[26] = { 0 };
+		for (int i = 0; i < s.size(); i++) {
+			x[s[i] - 'a']++;
+			x[t[i] - 'a']--;
+		
+		}
+
+
+		for (int i = 0; i < 26; i++) {
+			if (x[i] != 0) {
+
+				return false;
+			}
+
+			
+
+
+
+		}
+		return true;
+
+
+
+	}
+};
+
+
+
+int main() {
+	Solution so;
+	string s = "anagram", t = "nagaram";
+	bool result;
+	result = so.isAnagram(s, t);
+    cout << result<< endl;
+    return 0;
+}
+
