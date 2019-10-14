f185b7a9910376422b2f02b3c3b0f75ce6a5ede1
quanterk <51376997+quanterk@users.noreply.github.com>
2019-10-14 11:18:40 +0800

    Add files via upload

---
+++ 504.cpp
@@ -0,0 +1,68 @@
+
+#include <iostream>
+#include<vector>
+#include <algorithm>
+#include<string>
+using namespace std;
+
+
+
+class Solution {
+public:
+	string convertToBase7(int num) {
+		if (num == 0) return "0";
+		bool neg = false;
+		if (num < 0) {
+			neg = true;
+			num = -num;
+		}
+
+		string result = "";
+		int item = 0;
+		while (num) {
+			item = (num % 7);
+			cout << item << endl;
+			result.insert(0, 1, (char)(num % 7 + 48));
+			num = num/7;
+		}
+
+		if (neg) result.insert(0, 1, '-');
+
+		return result;
+
+
+
+
+
+
+	}
+};
+
+
+
+
+
+int main() {
+	Solution so;
+
+	
+	int num=100;
+	string result ;
+	result = so.convertToBase7(num);
+
+	for (auto num : result)
+		cout << num << endl;
+	cout << result << endl;
+
+
+	int test = 9;
+	cout << (char)(9+48) << endl;
+
+
+
+
+	//for (vector<int>::iterator i=nums.begin();i!=nums.end(),++i)
+
+	return 0;
+}
+
